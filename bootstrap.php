5e0964a9e5c354bda7e476c1b44bd78bd7bb8fe2
Terrence Howard <chemisus@gmail.com>
2012-07-25 23:32:49 -0400

    added bootstrap to start loading classes, and removed the require files from test

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @copyright   Copyright (c) 2012, Terrence Howard
+ */
+
+require_once dirname(__FILE__) . '/system/anaconda/src/Tree.php';
+require_once dirname(__FILE__) . '/system/anaconda/src/anaconda/Tree.php';
+require_once dirname(__FILE__) . '/system/anaconda/src/Vector.php';
+require_once dirname(__FILE__) . '/system/anaconda/src/anaconda/Vector.php';
