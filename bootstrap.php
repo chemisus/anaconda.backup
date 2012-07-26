fb7a6432ed898747cb7a763c1ad8433f2481f8da
Terrence Howard <chemisus@gmail.com>
2012-07-25 23:51:43 -0400

    added bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,8 @@
  * @copyright   Copyright (c) 2012, Terrence Howard
  */
 
-require_once dirname(__FILE__) . '/system/anaconda/src/Tree.php';
-require_once dirname(__FILE__) . '/system/anaconda/src/anaconda/Tree.php';
 require_once dirname(__FILE__) . '/system/anaconda/src/Vector.php';
 require_once dirname(__FILE__) . '/system/anaconda/src/anaconda/Vector.php';
+require_once dirname(__FILE__) . '/system/anaconda/src/Tree.php';
+require_once dirname(__FILE__) . '/system/anaconda/src/anaconda/Tree.php';
+
--- www/index.php
+++ www/index.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Anaconda PHP
- * 
- * @author      Terrence Howard <chemisus@gmail.com
- * @copyright   Terrence Howard, 2012
- * @version     0.0.1
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @copyright   Copyright (c) 2012, Terrence Howard
  */
 
+require_once('../bootstrap.php');
