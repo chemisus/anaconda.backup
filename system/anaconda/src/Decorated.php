d95d94a69b9a6e7d1d1c8f0d6c08eec4efb6f012
Terrence Howard <chemisus@gmail.com>
2012-07-27 16:53:01 -0400

    added decorated interface

---
+++ system/anaconda/src/Decorated.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @copyright   Copyright (c) 2012, Terrence Howard
+ */
+
+
+
+/**
+ * {@link \Decorated}
+ * 
+ * @package     
+ * @name        Decorated
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @version     0.1
+ * @since       0.1
+ */
+interface Decorated {
+    /**///<editor-fold desc="Properties">
+    /*\**********************************************************************\*/
+    /*\                             Properties                               \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+    function getBase();
+    /**///<editor-fold desc="Public Methods">
+    /*\**********************************************************************\*/
+    /*\                             Public Methods                           \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Event Handlers">
+    /*\**********************************************************************\*/
+    /*\                             Event Handlers                           \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+}
