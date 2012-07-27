0b85214d93bd81c182e76c180427c75e2b608cd6
Terrence Howard <chemisus@gmail.com>
2012-07-27 16:52:40 -0400

    wrapped bootstrap in a bootstrap class

--- bootstrap.php
+++ bootstrap.php
@@ -6,80 +6,83 @@
 
 define('ROOT', __DIR__.'/');
 
-call_user_func(function () {
-    require_once(ROOT.'system/anaconda/src/Autoloader.php');
+class Bootstrap {
+    public static function Run() {
+        require_once(ROOT.'system/anaconda/src/Autoloader.php');
 
-    require_once(ROOT.'system/anaconda/src/anaconda/Autoloader.php');
+        require_once(ROOT.'system/anaconda/src/anaconda/Autoloader.php');
 
-    \anaconda\Autoloader::Factory('autoload')
-    ->register()
-    ->addTranslate('\\', '{,_,/}')
-    ->addTranslate('_', '{,_,/}')
-    ->addExtension('.php')
-    ->add(
-        ROOT,
-        array(
-            'packages/*',
-            'application/*',
-            'system/*',
-        ),
-        'src'
-    );
-
-    \anaconda\Autoloader::Factory('view')
-    ->addTranslate('\\', '{,_,/}')
-    ->addTranslate('_', '{,_,/}')
-    ->addExtension('.xsl')
-    ->add(
-        ROOT,
-        array(
-            'packages/*',
-            'application/*',
-            'system/*',
-        ),
-        'view/*'
-    );
-
-    \anaconda\Autoloader::Factory('assets')
-    ->addTranslate('\\', '{,_,/}')
-    ->addTranslate('_', '{,_,/}')
-    ->add(
+        \anaconda\Autoloader::Factory('autoload')
+        ->register()
+        ->addTranslate('\\', '{,_,/}')
+        ->addTranslate('_', '{,_,/}')
+        ->addExtension('.php')
+        ->add(
             ROOT,
             array(
                 'packages/*',
                 'application/*',
                 'system/*',
             ),
-            'assets'
-        )->add(
+            'src'
+        );
+
+        \anaconda\Autoloader::Factory('view')
+        ->addTranslate('\\', '{,_,/}')
+        ->addTranslate('_', '{,_,/}')
+        ->addExtension('.xsl')
+        ->add(
             ROOT,
-            'www/assets'
-    );
+            array(
+                'packages/*',
+                'application/*',
+                'system/*',
+            ),
+            'view/*'
+        );
 
-    \anaconda\Autoloader::Factory('load')
-    ->addTranslate('\\', '{,_,/}')
-    ->addTranslate('_', '{,_,/}')
-    ->add(
-        ROOT,
-        array(
-            'system/*',
-            'application/*',
-            'packages/*',
-        )
-    );
-    
-    foreach (\anaconda\Autoloader::Get('load')->find('load.php') as $load) {
-        require_once($load);
+        \anaconda\Autoloader::Factory('assets')
+        ->addTranslate('\\', '{,_,/}')
+        ->addTranslate('_', '{,_,/}')
+        ->add(
+                ROOT,
+                array(
+                    'packages/*',
+                    'application/*',
+                    'system/*',
+                ),
+                'assets'
+            )->add(
+                ROOT,
+                'www/assets'
+        );
+
+        \anaconda\Autoloader::Factory('load')
+        ->addTranslate('\\', '{,_,/}')
+        ->addTranslate('_', '{,_,/}')
+        ->add(
+            ROOT,
+            array(
+                'system/*',
+                'application/*',
+                'packages/*',
+            )
+        );
+
+        foreach (\anaconda\Autoloader::Get('load')->find('load.php') as $load) {
+            require_once($load);
+        }
     }
-});
+}
 
+/*\**************************************************************************\*/
+/*\**************************************************************************\*/
+/*\**************************************************************************\*/
+/*\**************************************************************************\*/
+/*\**************************************************************************\*/
+/*\**************************************************************************\*/
 
-/*\**************************************************************************\*/
-/*\**************************************************************************\*/
-/*\**************************************************************************\*/
-/*\**************************************************************************\*/
-/*\**************************************************************************\*/
-/*\**************************************************************************\*/
+Bootstrap::Run();
 
 
 $subscribers = new anaconda\Tree();
@@ -88,19 +91,29 @@ $subscribers->setHeights(new anaconda\Vector());
 $subscribers->setNodes(new anaconda\Vector());
 $subscribers->setTrees(new anaconda\Vector());
 
-$subscribers->add(new anaconda\Subscriber());
-
 function publish(\Tree $tree, $node, $publisher) {
-    if (!$node->check($publisher) === false) {
+    if ($node->check($publisher) === false) {
         return;
     }
+    
+    $node->execute($publisher);
     
     foreach ($tree->getChildren($node) as $child) {
         publish($tree, $child, $publisher);
     }
 }
 
-$publisher = new anaconda\Publisher();
+$subscribers->add(
+    new anaconda\SubscriberSubset(array('name'=>'hi'),
+    new anaconda\SubscriberCallback(function (\Publisher $publisher) {
+})));
+
+
+$publisher = new anaconda\Publisher(array(
+    'name'=>'hi', 
+    'hey'=>'hey', 
+    'blah'=>array('ha'=>'he')
+));
 
 foreach ($subscribers->getRoots() as $subscriber) {
     publish($subscribers, $subscriber, $publisher);
