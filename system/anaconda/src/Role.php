51a4604ef6cf51b2330ba35f41e4a0fa73f35134
Terrence Howard <chemisus@gmail.com>
2012-07-31 20:50:20 -0400

    added interfaces, need to fill them

---
+++ system/anaconda/src/Role.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @copyright   Copyright (c) 2012, Terrence Howard
+ */
+
+
+
+/**
+ * {@link \Role}
+ * 
+ * @package     
+ * @name        Role
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @version     0.1
+ * @since       0.1
+ */
+interface Role {
+    /**///<editor-fold desc="Properties">
+    /*\**********************************************************************\*/
+    /*\                             Properties                               \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Public Methods">
+    /*\**********************************************************************\*/
+    /*\                             Public Methods                           \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Event Handlers">
+    /*\**********************************************************************\*/
+    /*\                             Event Handlers                           \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+}
---
+++ system/anaconda/src/Session.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @copyright   Copyright (c) 2012, Terrence Howard
+ */
+
+
+
+/**
+ * {@link \Session}
+ * 
+ * @package     
+ * @name        Session
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @version     0.1
+ * @since       0.1
+ */
+interface Session {
+    /**///<editor-fold desc="Properties">
+    /*\**********************************************************************\*/
+    /*\                             Properties                               \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Public Methods">
+    /*\**********************************************************************\*/
+    /*\                             Public Methods                           \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Event Handlers">
+    /*\**********************************************************************\*/
+    /*\                             Event Handlers                           \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+}
