c505e81d39649b62db52fc543bb9e887999d9f7a
Terrence Howard <chemisus@gmail.com>
2012-08-03 14:59:02 -0400

    RBAC added

---
+++ system/anaconda/src/anaconda/RoleTemplate.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * This file is part of Anaconda.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version accepted by Anaconda Ltd. in accordance with section
+ * 14 of the GNU General Public License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @copyright   Copyright (c) 2012, Terrence Howard
+ * @license     http://www.gnu.org/licenses/gpl.txt
+ *              GNU General Public License
+ */
+
+namespace anaconda;
+
+/**
+ * {@link anaconda\RoleTemplate}
+ * 
+ * @package     anaconda
+ * @name        RoleTemplate
+ * @author      Terrence Howard <chemisus@gmail.com>
+ * @version     0.1
+ * @since       0.1
+ */
+class RoleTemplate implements \anaconda\Subject, \anaconda\Role {
+    /**///<editor-fold desc="Constants">
+    /*\**********************************************************************\*/
+    /*\                             Constants                                \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Static Fields">
+    /*\**********************************************************************\*/
+    /*\                             Static Fields                            \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Static Methods">
+    /*\**********************************************************************\*/
+    /*\                             Static Methods                           \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Fields">
+    /*\**********************************************************************\*/
+    /*\                             Fields                                   \*/
+    /*\**********************************************************************\*/
+    private $subject;
+    
+    private $permissions = array();
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Properties">
+    /*\**********************************************************************\*/
+    /*\                             Properties                               \*/
+    /*\**********************************************************************\*/
+    public function permissions() {
+        return new \ArrayIterator($this->permissions);
+    }
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Constructors">
+    /*\**********************************************************************\*/
+    /*\                             Constructors                             \*/
+    /*\**********************************************************************\*/
+    public function __construct(\anaconda\Subject $subject) {
+        $this->subject = $subject;
+    }
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Private Methods">
+    /*\**********************************************************************\*/
+    /*\                             Private Methods                          \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Protected Methods">
+    /*\**********************************************************************\*/
+    /*\                             Protected Methods                        \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Public Methods">
+    /*\**********************************************************************\*/
+    /*\                             Public Methods                           \*/
+    /*\**********************************************************************\*/
+    public function addPermission(\anaconda\Permission $permission) {
+        $this->permissions[$permission->name()] = $permission;
+
+        return $permission;
+    }
+    
+    public function removePermission(\anaconda\Permission $permission) {
+        if (isset($this->permissions[$permission->name()])) {
+            unset($this->permissions[$permission->name()]);
+        }
+        
+        return $permission;
+    }
+    
+    public function check($permission, $value=null) {
+        if (isset($this->permissions[$permission])
+        && $this->permissions[$permission]->check($value)) {
+            return true;
+        }
+        
+        return $this->subject->check($permission, $value);
+    }
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Event Triggers">
+    /*\**********************************************************************\*/
+    /*\                             Event Triggers                           \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Event Handlers">
+    /*\**********************************************************************\*/
+    /*\                             Event Handlers                           \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+
+    /**///<editor-fold desc="Classes">
+    /*\**********************************************************************\*/
+    /*\                             Classes                                  \*/
+    /*\**********************************************************************\*/
+    /**///</editor-fold>
+}
